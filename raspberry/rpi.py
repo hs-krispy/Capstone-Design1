0b080cb5a212f9392a4169679282b383258cc07d
dlrgy22 <dlrgy22@gmail.com>
2020-11-11 23:06:02 +0900

    카메라 설정

--- raspberry/rpi.py
+++ raspberry/rpi.py
@@ -2,37 +2,39 @@ import cv2
 import find_face
 import use_model
 from keras.models import load_model
+import time
 
-white_model = load_model('black_mask.h5')
+model = load_model('mask_model.h5')
+#black_model = load_model('black_mask.h5')
 capture = cv2.VideoCapture(0)
 
 while True:
     find = False
-    while not find:
-        ret, frame = capture.read()
-        cv2.imshow("VideoFrame", frame)
-        find = find_face.img_processing('./sv_img/face.jpg', frame)
-
-        if cv2.waitKey(1) > 0:
+    ret, frame = capture.read()
+    cv2.imshow("VideoFrame", frame)
+    find = find_face.img_processing('./sv_img/face.jpg', frame)
+    if cv2.waitKey(1) > 0:
             break
-    pred = use_model.predict_mask(white_model)
 
-    print(pred)
+    if not find :
+        continue
+
+
+    pred = use_model.predict_mask(model)
+
     if pred == 1:
         print("pass")
 
-
-
-    elif pred == 0:
+    else :
         print('please wear a mask')
 
-
-
-    print("find face please enter next person")
-
-    sig = input()
-    if sig == 'exit':
-        break
+    start_time = time.time()
+    while time.time() - start_time < 5:
+        ret, frame = capture.read()
+        cv2.imshow("VideoFrame", frame)
+        if cv2.waitKey(1) > 0:
+            break
+        # 여기에 테스트 배드 코드
 
 
 capture.release()
