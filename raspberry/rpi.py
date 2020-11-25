4a86ea65d1f54f7ab39efc7c1c373ee766f2b2d2
hs-krispy <58063806+hs-krispy@users.noreply.github.com>
2020-11-25 14:28:13 +0900

    Add Testbed code

--- raspberry/rpi.py
+++ raspberry/rpi.py
@@ -1,41 +1,50 @@
 import cv2
 import find_face
 import use_model
-from keras.models import load_model
+from imutils.video import VideoStream
+from tensorflow.keras.models import load_model
 import time
+import serial
 
-model = load_model('mask_model.h5')
-#black_model = load_model('black_mask.h5')
-capture = cv2.VideoCapture(0)
+ser = serial.Serial('/dev/ttyACM0', 9600)
+model = load_model('final_mask.h5')
+capture = VideoStream(usePiCamera=True, resolution=(640, 480)).start()
+time.sleep(2.0)
 
 while True:
     find = False
-    ret, frame = capture.read()
+    frame = capture.read()
     cv2.imshow("VideoFrame", frame)
     find = find_face.img_processing('./sv_img/face.jpg', frame)
     if cv2.waitKey(1) > 0:
-            break
+        break
 
-    if not find :
+    if not find:
         continue
 
-
     pred = use_model.predict_mask(model)
-
     if pred == 1:
         print("pass")
+        c = 'y'
+        c = c.encode('utf-8')
+        ser.write(c)
+        while True:
+            if ser.readable():
+                res = ser.readline()
+                print(res.decode()[:len(res) - 1])
+            break
 
-    else :
+    else:
         print('please wear a mask')
 
     start_time = time.time()
+
     while time.time() - start_time < 5:
-        ret, frame = capture.read()
+        frame = capture.read()
         cv2.imshow("VideoFrame", frame)
         if cv2.waitKey(1) > 0:
             break
-        # 여기에 테스트 배드 코드
-
 
 capture.release()
+
 cv2.destroyAllWindows()
